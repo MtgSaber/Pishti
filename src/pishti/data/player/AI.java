733a2b2bfeba4ba31b97bd23f1055d8a2541d55f
Andrew <andrewja314@gmail.com>
2017-03-30 19:19:05 -0400

    Purged single-list wrappers and junk
    All that remains is the completed card package and testing.CardImg.class

--- src/pishti/data/player/AI.java
+++
@@ -1,7 +0,0 @@
-package pishti.data.player;
-
-/**
- * Author: Andrew Arnold (3/28/2017)
- */
-public class AI extends Player {
-}
--- src/pishti/gui/panes/PlayerPane.java
+++
@@ -1,9 +0,0 @@
-package pishti.gui.panes;
-
-import pishti.data.player.User;
-
-/**
- * Author: Andrew Arnold (3/28/2017)
- */
-public class PlayerPane extends User {
-}
