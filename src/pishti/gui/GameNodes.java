7fd98e6fc89fa74a1048bd62c7f0422039c021bc
Andrew <andrewja314@gmail.com>
2017-04-19 14:22:23 -0400

    Updated GameNodes class

--- src/pishti/gui/GameNodes.java
+++ src/pishti/gui/GameNodes.java
@@ -1,13 +1,8 @@
 package pishti.gui;
 
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 /**
  * Author: Andrew Arnold (4/13/2017)
@@ -16,15 +11,13 @@ import javafx.stage.Stage;
  * Serves as a resource wrapper for more organized upper-level code.
  */
 public class GameNodes {
-    private BorderPane pnPrimary;
     private HBox handPlayer;
     private HBox handAI;
-    private VBox capturedPlayer;
-    private VBox capturedAI;
-    private VBox deck;
-    private VBox discard;
+    private ImageView capturedPlayer;
+    private ImageView capturedAI;
+    private ImageView deck;
+    private ImageView discard;
 
-    private Button btDeal;
     private Text capturedPlayerCnt;
     private Text capturedAICnt;
     private Text capturedPlayerVal;
@@ -34,32 +27,28 @@ public class GameNodes {
     private Text discardVal;
 
     public GameNodes() {
-        pnPrimary = new BorderPane();
-        capturedPlayer = new VBox(5);
-        capturedAI = new VBox(5);
         handPlayer = new HBox(5);
         handAI = new HBox(5);
-        deck = new VBox(5);
-        discard = new VBox(5);
+        capturedPlayer = new ImageView();
+        capturedAI = new ImageView();
+        deck = new ImageView();
+        discard = new ImageView();
 
-        btDeal = new Button("Draw");
         capturedPlayerCnt = new Text("0");
         capturedAICnt = new Text("0");
         capturedPlayerVal = new Text("0");
         capturedAIVal = new Text("0");
-        deckCnt = new Text("48");
-        discardCnt = new Text("4");
+        deckCnt = new Text("0");
+        discardCnt = new Text("0");
         discardVal = new Text("0");
     }
 
-    public BorderPane getPnPrimary() { return pnPrimary; }
     public HBox getHandPlayer() { return handPlayer; }
     public HBox getHandAI() { return handAI; }
-    public VBox getCapturedPlayer() { return capturedPlayer; }
-    public VBox getCapturedAI() { return capturedAI; }
-    public VBox getDeck() { return deck; }
-    public VBox getDiscard() { return discard; }
-    public Button getBtDeal() { return btDeal; }
+    public ImageView getCapturedPlayer() { return capturedPlayer; }
+    public ImageView getCapturedAI() { return capturedAI; }
+    public ImageView getDeck() { return deck; }
+    public ImageView getDiscard() { return discard; }
     public Text getCapturedPlayerCnt() { return capturedPlayerCnt; }
     public Text getCapturedAICnt() { return capturedAICnt; }
     public Text getCapturedPlayerVal() { return capturedPlayerVal; }
